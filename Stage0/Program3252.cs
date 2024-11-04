03baf2df677d0788a87160a0c386da7941e82ccc
oriyari2 <oyitzhak@g.jct.ac.il>
2024-11-04 11:09:28 +0200

    Initial Code + Welcom functions

---
+++ Stage0/Program3252.cs
@@ -0,0 +1,18 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome3252();
+        Welcome1013();
+        Console.ReadKey();
+    }
+
+    private static void Welcome3252()
+    {
+        Console.Write("Enter your name: ");
+        string? name = Console.ReadLine();//we add ? in order to prevent problems of enter null to string
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+    static partial void Welcome1013();
+    
+}
