cd3976b73c063a3f639f126545c6c48ebc59806b
oriyari2 <oyitzhak@g.jct.ac.il>
2024-12-23 20:29:35 +0200

    j

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -16,9 +16,75 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        public DateTime CurrentTime
+        {
+            get { return (DateTime)GetValue(CurrentTimeProperty); }
+            set { SetValue(CurrentTimeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CurrentTime.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CurrentTimeProperty =
+            DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow));
+
+        private void btnAddOneMinute_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.Minute);
+        }
+        private void btnAddOneHour_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.Hour);
+        }
+        private void btnAddOneDay_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.Day);
+        }
+        private void btnAddOneMonth_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.Month);
+        }
+        private void btnAddOneYear_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.Year);
+        }
+
+        public TimeSpan CurrentRiskRange
+        {
+            get { return (TimeSpan)GetValue(CurrentRiskRangeProperty); }
+            set { SetValue(CurrentRiskRangeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CurrentRiskRange.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CurrentRiskRangeProperty =
+            DependencyProperty.Register("CurrentRiskRange", typeof(TimeSpan), typeof(MainWindow));
+
+        private void btnUpdateRiskRange_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.SetRiskRange(CurrentRiskRange);
+        }
+
+        private void clockObserver() //maybe no need parameters
+        {
+            CurrentTime = s_bl.Admin.GetClock();
+        }
+
+        private void configObserver() //maybe no need parameters
+        {
+            CurrentRiskRange = s_bl.Admin.GetRiskRange();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            CurrentTime = s_bl.Admin.GetClock();
+            CurrentRiskRange = s_bl.Admin.GetRiskRange();
+            s_bl.Admin.AddClockObserver(clockObserver);
+            s_bl.Admin.AddConfigObserver(configObserver);
+        }
+
     }
 }
--- PL/Volunteer/VolunteerListWindow.xaml.cs
+++ PL/Volunteer/VolunteerListWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace PL.Volunteer
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
